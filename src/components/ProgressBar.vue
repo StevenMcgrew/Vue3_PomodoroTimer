ebd8e8a74dac9b50f8aa6b5f21220fc0eff510aa
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-02-07 20:10:15 -0800

    Working on progress bar

---
+++ src/components/ProgressBar.vue
@@ -0,0 +1,32 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="progress-bar">
+    <span class="work-block">work</span>
+    <span class="break-block">break</span>
+    <span class="work-block">work</span>
+    <span class="break-block">break</span>
+    <span class="work-block">work</span>
+    <span class="break-block">break</span>
+    <span class="work-block">work</span>
+    <span class="long-break-block">long break</span>
+</div>
+</template>
+
+<style scoped>
+
+.progress-bar {
+    width: 100%;
+    background-color: lightgray;
+    height: fit-content;
+    margin: 20rem;
+    display: grid;
+    justify-items: center;
+    grid-template-columns: auto auto auto auto auto auto auto auto;
+}
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <script setup>
 
-import Counter from "../components/Counter.vue"
-import PlayPauseBtn from "../components/PlayPauseBtn.vue"
+import ProgressBar from '../components/ProgressBar.vue'
+import Counter from '../components/Counter.vue'
+import PlayPauseBtn from '../components/PlayPauseBtn.vue'
 
 </script>
 
 <template>
     <div class="home">
-
+        <div class="graphic">
+            <ProgressBar />
+        </div>
         <div class="counter">
             <Counter />
             <PlayPauseBtn />
@@ -19,17 +22,18 @@ import PlayPauseBtn from "../components/PlayPauseBtn.vue"
 .home {
     width: 100%;
     display: grid;
-    grid-template-rows: auto 1fr auto;
+    grid-template-rows: 1fr auto;
     grid-template-areas:
-        "settings"
         "graphic"
         "counter";
+}
+.graphic {
+    grid-area: graphic;
+    display: flex;
+    align-items: center;
 }
 .counter {
     grid-area: counter;
     justify-self: center;
-}
-.settings {
-    grid-area: settings;
 }
 </style>
