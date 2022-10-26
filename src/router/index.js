9a6246e0a5f30a9b1cf7a91e8a75ab084559cb5c
StevenMcgrew <stevenmcgrew8224@msn.com>
2022-10-26 14:04:47 -0700

    Change router to createWebHashHistory() mode to prevent 404 errors from github pages

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory } from "vue-router"
 import About from '../views/About.vue'
 import Home from '../views/Home.vue'
 import Settings from '../views/Settings.vue'
@@ -22,7 +22,7 @@ const routes = [
 ]
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHashHistory(),
     routes
 })
 
